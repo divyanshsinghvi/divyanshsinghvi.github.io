472a8baf5270a61fc5aafb8e3b8901a7fa06a07b
divyanshsinghvi <divyanshsinghvi@gmail.com>
2025-09-14 05:27:50 +0000

    Deploying to gh-pages from @ divyanshsinghvi/divyanshsinghvi.github.io@5824e8877bdf30cfe07ace35d9c79ca29e84ac08 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -77,7 +77,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%69%76%79%61%6E%73%68%73%69%6E%67%68%76%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -98,13 +98,13 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=7AaPJF8AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.divyanshsinghvi.github.io/", "_blank");
         },
       },];
